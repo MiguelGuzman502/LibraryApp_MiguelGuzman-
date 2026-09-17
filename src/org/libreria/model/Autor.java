97e3cd6da5c650dc1bc87c1a4f34ba831b2b389a
Miguel Guzman <mguzman-2023183@kinal.edu.gt>
2026-09-17 12:03:26 -0600

    se agrego la clase de autor al paquete de model

---
+++ src/org/libreria/model/Autor.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.libreria.model;
+
+/**
+ *
+ * @author miguel guzman 
+ * @version 1.0.0
+ * @see org.libreria.model.Autor
+ */
+public class Autor {
+ private int idAutor;
+ private String nombreAutor;
+ private String apellidoAutor;
+ private String nacionalidad;
+ private String biografia;
+ 
+ public Autor(){
+     
+ }
+ public Autor(int idAutor,String nombreAutor, String apellidoAutor, String nacionalidad, String biografia){
+     this.idAutor = idAutor;
+     this.nombreAutor = nombreAutor;
+     this.apellidoAutor = apellidoAutor;
+     this.nacionalidad = nacionalidad;
+     this.biografia = biografia; 
+     
+ }
+ public int getIdAutor(){
+     return idAutor;
+    
+ }
+ public void setIdAutor(){
+     this.idAutor = idAutor;
+ }
+ public String getNombreAutor(){
+     return nombreAutor;
+     
+ }
+ public void setNombreAutor(String nombreAutor){
+     this.nombreAutor = nombreAutor;
+     
+ }
+ 
+ public String getApellidoAutor(){
+     return apellidoAutor;
+ }
+ 
+ public void setApellidoAutor(String apellidoAutor){
+     this.apellidoAutor = apellidoAutor;
+ }
+ public String getNacionalidad(){
+     return nacionalidad;
+ }
+ 
+ public void setNacionalidad(String nacionalidad){
+     this.nacionalidad = nacionalidad;
+ }
+  public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+
+    @Override
+    public String toString() {
+        return nombreAutor + " " + apellidoAutor;
+    }
+}
+
