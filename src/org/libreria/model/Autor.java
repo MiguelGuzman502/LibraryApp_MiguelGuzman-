c15f6d1a9389e426e4bcad9e902673058b41fda3
Miguel Guzman <mguzman-2023183@kinal.edu.gt>
2026-09-18 10:14:59 -0600

    se agrego la clase Cliente en su respectiva rama y se agrego el java doc en Autor

--- src/org/libreria/model/Autor.java
+++ src/org/libreria/model/Autor.java
@@ -2,69 +2,143 @@
 package org.libreria.model;
 
 /**
+ * Representa un autor dentro del sistema de librería.
+ * Contiene la información del autor, incluyendo su nombre,
+ * apellido, nacionalidad y biografía.
  *
- * @author miguel guzman 
+ * @author Miguel Guzman
  * @version 1.0.0
- * @see org.libreria.model.Autor
  */
 public class Autor {
- private int idAutor;
- private String nombreAutor;
- private String apellidoAutor;
- private String nacionalidad;
- private String biografia;
- 
- public Autor(){
-     
- }
- public Autor(int idAutor,String nombreAutor, String apellidoAutor, String nacionalidad, String biografia){
-     this.idAutor = idAutor;
-     this.nombreAutor = nombreAutor;
-     this.apellidoAutor = apellidoAutor;
-     this.nacionalidad = nacionalidad;
-     this.biografia = biografia; 
-     
- }
- public int getIdAutor(){
-     return idAutor;
-    
- }
- public void setIdAutor(){
-     this.idAutor = idAutor;
- }
- public String getNombreAutor(){
-     return nombreAutor;
-     
- }
- public void setNombreAutor(String nombreAutor){
-     this.nombreAutor = nombreAutor;
-     
- }
- 
- public String getApellidoAutor(){
-     return apellidoAutor;
- }
- 
- public void setApellidoAutor(String apellidoAutor){
-     this.apellidoAutor = apellidoAutor;
- }
- public String getNacionalidad(){
-     return nacionalidad;
- }
- 
- public void setNacionalidad(String nacionalidad){
-     this.nacionalidad = nacionalidad;
- }
-  public String getBiografia() {
+
+    private int idAutor;
+    private String nombreAutor;
+    private String apellidoAutor;
+    private String nacionalidad;
+    private String biografia;
+
+    /**
+     * Crea un objeto Autor sin establecer sus valores.
+     */
+    public Autor() {
+    }
+
+    /**
+     * Crea un objeto Autor con la información proporcionada.
+     *
+     * @param idAutor Identificador del autor.
+     * @param nombreAutor Nombre del autor.
+     * @param apellidoAutor Apellido del autor.
+     * @param nacionalidad Nacionalidad del autor.
+     * @param biografia Información biográfica del autor.
+     */
+    public Autor(int idAutor, String nombreAutor, String apellidoAutor,
+                 String nacionalidad, String biografia) {
+        this.idAutor = idAutor;
+        this.nombreAutor = nombreAutor;
+        this.apellidoAutor = apellidoAutor;
+        this.nacionalidad = nacionalidad;
+        this.biografia = biografia;
+    }
+
+    /**
+     * Obtiene el identificador del autor.
+     *
+     * @return El identificador del autor.
+     */
+    public int getIdAutor() {
+        return idAutor;
+    }
+
+    /**
+     * Modifica el identificador del autor.
+     *
+     * @param idAutor Nuevo identificador del autor.
+     */
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    /**
+     * Obtiene el nombre del autor.
+     *
+     * @return El nombre del autor.
+     */
+    public String getNombreAutor() {
+        return nombreAutor;
+    }
+
+    /**
+     * Modifica el nombre del autor.
+     *
+     * @param nombreAutor Nuevo nombre del autor.
+     */
+    public void setNombreAutor(String nombreAutor) {
+        this.nombreAutor = nombreAutor;
+    }
+
+    /**
+     * Obtiene el apellido del autor.
+     *
+     * @return El apellido del autor.
+     */
+    public String getApellidoAutor() {
+        return apellidoAutor;
+    }
+
+    /**
+     * Modifica el apellido del autor.
+     *
+     * @param apellidoAutor Nuevo apellido del autor.
+     */
+    public void setApellidoAutor(String apellidoAutor) {
+        this.apellidoAutor = apellidoAutor;
+    }
+
+    /**
+     * Obtiene la nacionalidad del autor.
+     *
+     * @return La nacionalidad del autor.
+     */
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    /**
+     * Modifica la nacionalidad del autor.
+     *
+     * @param nacionalidad Nueva nacionalidad del autor.
+     */
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    /**
+     * Obtiene la biografía del autor.
+     *
+     * @return La biografía del autor.
+     */
+    public String getBiografia() {
         return biografia;
     }
 
+    /**
+     * Modifica la biografía del autor.
+     *
+     * @param biografia Nueva biografía del autor.
+     */
     public void setBiografia(String biografia) {
         this.biografia = biografia;
     }
 
+    /**
+     * Devuelve el nombre completo del autor como representación textual del objeto.
+     *
+     * @return El nombre y apellido del autor.
+     */
     @Override
     public String toString() {
         return nombreAutor + " " + apellidoAutor;
     }
 }
+
---
+++ src/org/libreria/model/Cliente.java
@@ -0,0 +1,121 @@
+
+package org.libreria.model;
+
+/**
+ * Representa un cliente dentro del sistema de librería.
+ * Contiene la información personal y de contacto del cliente,
+ * incluyendo su CUI, nombre, apellido y correo electrónico.
+ *
+ * @author Miguel Guzman
+ * @version 1.0.0
+ */
+public class Cliente {
+
+    private long cui;
+    private String nombreCliente;
+    private String apellidoCliente;
+    private String correoElectronico;
+
+    /**
+     * Crea un objeto Cliente sin establecer sus valores.
+     */
+    public Cliente() {
+    }
+
+    /**
+     * Crea un objeto Cliente con la información proporcionada.
+     *
+     * @param cui CUI que identifica al cliente.
+     * @param nombreCliente Nombre del cliente.
+     * @param apellidoCliente Apellido del cliente.
+     * @param correoElectronico Correo electrónico del cliente.
+     */
+    public Cliente(long cui, String nombreCliente, String apellidoCliente, String correoElectronico) {
+        this.cui = cui;
+        this.nombreCliente = nombreCliente;
+        this.apellidoCliente = apellidoCliente;
+        this.correoElectronico = correoElectronico;
+    }
+
+    /**
+     * Obtiene el CUI del cliente.
+     *
+     * @return El CUI del cliente.
+     */
+    public long getCui() {
+        return cui;
+    }
+
+    /**
+     * Modifica el CUI del cliente.
+     *
+     * @param cui Nuevo CUI del cliente.
+     */
+    public void setCui(long cui) {
+        this.cui = cui;
+    }
+
+    /**
+     * Obtiene el nombre del cliente.
+     *
+     * @return El nombre del cliente.
+     */
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    /**
+     * Modifica el nombre del cliente.
+     *
+     * @param nombreCliente Nuevo nombre del cliente.
+     */
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    /**
+     * Obtiene el apellido del cliente.
+     *
+     * @return El apellido del cliente.
+     */
+    public String getApellidoCliente() {
+        return apellidoCliente;
+    }
+
+    /**
+     * Modifica el apellido del cliente.
+     *
+     * @param apellidoCliente Nuevo apellido del cliente.
+     */
+    public void setApellidoCliente(String apellidoCliente) {
+        this.apellidoCliente = apellidoCliente;
+    }
+
+    /**
+     * Obtiene el correo electrónico del cliente.
+     *
+     * @return El correo electrónico del cliente.
+     */
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    /**
+     * Modifica el correo electrónico del cliente.
+     *
+     * @param correoElectronico Nuevo correo electrónico del cliente.
+     */
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    /**
+     * Devuelve el nombre completo del cliente como representación textual del objeto.
+     *
+     * @return El nombre y apellido del cliente.
+     */
+    @Override
+    public String toString() {
+        return nombreCliente + " " + apellidoCliente;
+    }
+}
