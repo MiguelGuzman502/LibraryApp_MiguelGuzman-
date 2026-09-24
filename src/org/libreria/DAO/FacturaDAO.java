f018a70186123c4e254d0f292a150c04d8b92fa1
Miguel Guzman <mguzman-2023183@kinal.edu.gt>
2026-09-24 17:31:20 -0600

    se agrego en la  clase de FacturaDao la estrctura del javadoc

--- src/org/libreria/DAO/FacturaDAO.java
+++ src/org/libreria/DAO/FacturaDAO.java
@@ -1,10 +1,24 @@
-
 package org.libreria.DAO;
-
 
 import java.util.ArrayList;
 import org.libreria.model.LineaFactura;
 
+/**
+ * Interfaz de acceso a datos para la consulta de facturas.
+ * Define las operaciones necesarias para obtener la información
+ * de una factura mediante el número de venta.
+ * @author Miguel Guzman
+ * @version 1.0.0
+ * @see LineaFactura
+ */
 public interface FacturaDAO {
+
+    /**
+     * Busca la información de una factura utilizando
+     * el número de venta.
+     * @param noVenta Número que identifica la venta asociada
+     *                a la factura.
+     * @return Lista de líneas que contienen la información de la factura.
+     */
     ArrayList<LineaFactura> buscarFactura(int noVenta);
 }
