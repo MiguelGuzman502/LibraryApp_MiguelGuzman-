bdcb06cef99fd1c9b625d0921cfc4376675e5343
Miguel Guzman <mguzman-2023183@kinal.edu.gt>
2026-09-24 11:27:37 -0600

    se agrego en la clase de dao el javadoc

--- src/org/libreria/DAO/Dao.java
+++ src/org/libreria/DAO/Dao.java
@@ -2,8 +2,17 @@
 package org.libreria.DAO;
 
 /**
- *
+ *es una interfz para el acceso a datos de desde las entidades del sistema
+ * es una creacion, conuslta, actualizacion y eliminacion de datos
+ * /**
+ * Interfaz base para el acceso a datos de las entidades del sistema.
+ 
+ * @param <T> 
+ * @param <K> 
+ 
  * @author Miguel Guzman 
+ * @version 1.0.0
+ * @see Crud
  */
 public interface Dao<T, K> extends Crud<T, K> {
 }
