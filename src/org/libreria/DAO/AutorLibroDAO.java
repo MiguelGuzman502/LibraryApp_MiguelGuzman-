42ae96d1ec60898618cdfd24007f808d71a4f16d
Miguel Guzman <mguzman-2023183@kinal.edu.gt>
2026-09-24 16:36:13 -0600

    se agrego en la  clase de AutorLibroDAO la estructura del javadoc

--- src/org/libreria/DAO/AutorLibroDAO.java
+++ src/org/libreria/DAO/AutorLibroDAO.java
@@ -3,6 +3,14 @@ package org.libreria.DAO;
 
 import org.libreria.model.AutorLibro;
 
-public interface AutorLibroDAO extends Crud<AutorLibro, Integer>{
-    
+/**
+ * Interfaz de acceso a datos para la entidad {@link AutorLibro}.
+ * Hereda las operaciones CRUD definidas en {@link Crud}.
+ * @author Miguel Guzman
+ * @version 1.0.0
+ * @see AutorLibro
+ * @see Crud
+ */
+public interface AutorLibroDAO extends Crud<AutorLibro, Integer> {
+
 }
