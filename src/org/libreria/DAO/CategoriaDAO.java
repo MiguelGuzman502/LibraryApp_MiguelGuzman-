48a4165ff73e18afa43fbef5cf9ffe65bfa079b4
Miguel Guzman <mguzman-2023183@kinal.edu.gt>
2026-09-24 16:49:31 -0600

    se agrego en la  clase de CategoriaDao la estrctura del javadoc

--- src/org/libreria/DAO/CategoriaDAO.java
+++ src/org/libreria/DAO/CategoriaDAO.java
@@ -1,11 +1,15 @@
-
 package org.libreria.DAO;
+
 import org.libreria.model.Categoria;
 
 /**
- *
- * @author aruba
+ * Interfaz de acceso a datos para la entidad {@link Categoria}.
+ * Hereda las operaciones CRUD definidas en {@link Crud}.
+ * @author  Miguel Guzman
+ * @version 1.0.0
+ * @see Categoria
+ * @see Crud
  */
-public interface CategoriaDAO extends Crud<Categoria, Integer>{
-    
+public interface CategoriaDAO extends Crud<Categoria, Integer> {
+
 }
