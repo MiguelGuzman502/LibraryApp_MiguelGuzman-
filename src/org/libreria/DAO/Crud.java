54bd6f1ad817bb82af5af0646abc080a325afbb0
Miguel Guzman <mguzman-2023183@kinal.edu.gt>
2026-09-24 11:51:52 -0600

    se agrego en la clase de Cru y se escribio el codigo

---
+++ src/org/libreria/DAO/Crud.java
@@ -0,0 +1,12 @@
+
+package org.libreria.DAO;
+
+import java.util.ArrayList;
+
+public interface Crud<T, K> {
+    boolean crear(T entidad);
+    boolean actualizar(T entidad);
+    boolean eliminar(K id);
+    T buscarPorId(K id);
+    ArrayList<T> listarTodos();
+}
