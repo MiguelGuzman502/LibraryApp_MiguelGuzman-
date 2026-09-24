7d26b1829aa7ec284c2d58184c05c2169a8b12e7
Miguel Guzman <mguzman-2023183@kinal.edu.gt>
2026-09-24 16:14:10 -0600

    se agrego en la clase  AutorDao el formato de javadoc

--- src/org/libreria/DAO/AutorDao.java
+++ src/org/libreria/DAO/AutorDao.java
@@ -2,7 +2,14 @@ package org.libreria.DAO;
 
 import org.libreria.model.Autor;
 
+/**
+ * Interfaz de acceso a datos para la entidad {@link Autor}.
+ * Hereda las operaciones CRUD definidas en {@link Crud}.
+ * @author Miguel Guzman
+ * @version 1.0.0
+ * @see Autor
+ * @see Crud
+ */
+public interface AutorDao extends Crud<Autor, Integer> {
 
-public interface AutorDao extends Crud<Autor, Integer>{
-    
 }
