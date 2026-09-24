5cda3a530db598a4a18fa55238bd9fb1ebdc879d
Miguel Guzman <mguzman-2023183@kinal.edu.gt>
2026-09-24 17:22:03 -0600

    se agrego en la  clase de EditorialDao con la estructura de javadoc

--- src/org/libreria/DAO/EditorialDAO.java
+++ src/org/libreria/DAO/EditorialDAO.java
@@ -1,10 +1,15 @@
 package org.libreria.DAO;
+
 import org.libreria.model.Editorial;
 
 /**
- *
- * @author aruba
+ * Interfaz de acceso a datos para la entidad {@link Editorial}.
+ * Hereda las operaciones CRUD definidas en {@link Crud}.
+ * @author Miguel Guzman
+ * @version 1.0.0
+ * @see Editorial
+ * @see Crud
  */
-public interface EditorialDAO extends Crud<Editorial, String>{
-    
+public interface EditorialDAO extends Crud<Editorial, String> {
+
 }
